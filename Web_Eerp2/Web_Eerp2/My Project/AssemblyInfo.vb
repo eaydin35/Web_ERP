ade35691061a01f1b5d4442f0bbf217f5eef228b
Emrullah Aydın <52963231+eaydin35@users.noreply.github.com>
2021-06-06 19:52:41 +0300

    v0.1

---
+++ Web_Eerp2/Web_Eerp2/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+' denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+' öznitelik değerlerini değiştirin.
+
+' Derleme özniteliklerinin değerlerini incele
+<Assembly: AssemblyTitle("Web_Eerp2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Web_Eerp2")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Aşağıdaki GUID, bu proje COM'a gösterildiğinde typelib Kimliği içindir.
+<Assembly: Guid("1edc70b2-5571-4267-9ccf-05bcbd86cda6")>
+
+' Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Tüm değerleri belirtebilirsiniz veya Yapı ve Düzeltme Numaralarına aşağıda 
+' gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
